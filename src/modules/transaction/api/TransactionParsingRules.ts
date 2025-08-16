6ef8a66b2399459f9c6d8247ecd7d09581f591c5
daxiong <vnpay@pron.me>
2025-08-16 11:10:06 +0800

    feat(transaction): 优化交易解析规则功能
    - 添加解析结果字段和相关国际化
    - 实现正则表达式解析功能
    - 优化表单组件，支持通道选择和状态选择
    - 修复一些代码格式和类型定义问题

--- src/modules/transaction/api/TransactionParsingRules.ts
+++ src/modules/transaction/api/TransactionParsingRules.ts
@@ -1,42 +1,60 @@
-import type { ResponseStruct } from '#/global'
+import type { ResponseStruct } from "#/global";
 
 export interface TransactionParsingRulesVo {
   // 自增id
-  id: string
+  id: string;
   // 渠道ID
-  channel_id: string
+  channel_id: string;
   // 正则表达式
-  regex: string
+  regex: string;
   // 提取变量名
-  variable_name: string
+  variable_name: string;
   // 示例数据
-  example_data: string
+  example_data: string;
   // 状态：1启用 0禁用
-  status: string
+  status: string;
   // 创建时间
-  created_at: string
+  created_at: string;
   // 更新时间
-  updated_at: string
+  updated_at: string;
   // 删除时间
-  deleted_at: string
+  deleted_at: string;
+  parse_result: Array<{
+    label: string;
+    value: string | number;
+  }>;
 }
 
 // 解析规则查询
-export function page(params: TransactionParsingRulesVo): Promise<ResponseStruct<TransactionParsingRulesVo[]>> {
-return useHttp().get('/admin/transaction/transaction_parsing_rules/list', { params })
+export function page(
+  params: TransactionParsingRulesVo
+): Promise<ResponseStruct<TransactionParsingRulesVo[]>> {
+  return useHttp().get("/admin/transaction/transaction_parsing_rules/list", {
+    params,
+  });
 }
 
 // 解析规则新增
-export function create(data: TransactionParsingRulesVo): Promise<ResponseStruct<null>> {
-  return useHttp().post('/admin/transaction/transaction_parsing_rules', data)
+export function create(
+  data: TransactionParsingRulesVo
+): Promise<ResponseStruct<null>> {
+  return useHttp().post("/admin/transaction/transaction_parsing_rules", data);
 }
 
 // 解析规则编辑
-export function save(id: number, data: TransactionParsingRulesVo): Promise<ResponseStruct<null>> {
-    return useHttp().put(`/admin/transaction/transaction_parsing_rules/${id}`, data)
+export function save(
+  id: number,
+  data: TransactionParsingRulesVo
+): Promise<ResponseStruct<null>> {
+  return useHttp().put(
+    `/admin/transaction/transaction_parsing_rules/${id}`,
+    data
+  );
 }
 
 // 解析规则删除
 export function deleteByIds(ids: number[]): Promise<ResponseStruct<null>> {
-      return useHttp().delete('/admin/transaction/transaction_parsing_rules', { data: ids })
+  return useHttp().delete("/admin/transaction/transaction_parsing_rules", {
+    data: ids,
+  });
 }
--- src/modules/transaction/views/TransactionParsingRules/components/GetFormItems.tsx
+++ src/modules/transaction/views/TransactionParsingRules/components/GetFormItems.tsx
@@ -9,7 +9,43 @@
  */
 import type { MaFormItem } from "@mineadmin/form";
 import type { TransactionParsingRulesVo } from "~/transaction/api/TransactionParsingRules.ts";
+import type { ChannelDictVo } from "~/channel/api/Channel.ts";
+import { remote } from "~/channel/api/Channel.ts";
+import { selectStatus } from "@/modules/Common";
+import MaKeyValue from "@/components/ma-key-value/index.vue";
 
+const resultArr = ref<{ variable_name: string; result: string | number }[]>([]);
+const matchResult = ref<string[]>([]);
+
+const runRegex = (
+  newExData: string,
+  newRegex: string,
+  newValName: string[]
+) => {
+  const regex = new RegExp(newRegex);
+  const match = newExData.match(regex);
+  if (match) {
+    // 过滤 match[0] 并过滤空值
+    match.shift();
+    matchResult.value = match.filter(
+      (item): item is string => item !== undefined
+    );
+
+    if (newValName.length !== matchResult.value.length) {
+      resultArr.value = [];
+      return;
+    }
+
+    const result = Array<{ variable_name: string; result: string | number }>();
+    for (let i = 0; i < newValName.length; i++) {
+      result.push({
+        variable_name: newValName[i],
+        result: matchResult.value[i],
+      });
+    }
+    resultArr.value = result;
+  }
+};
 export default function getFormItems(
   formType: "add" | "edit" = "add",
   t: any,
@@ -24,32 +60,97 @@ export default function getFormItems(
   if (formType === "edit") {
     // todo...
   }
+  model.parse_result = [
+    {
+      label: "变量名",
+      value: "变量值",
+    },
+  ];
 
   return [
     {
       label: t("TransactionParsingRules.channel_id"),
       prop: "channel_id",
-      render: () => <el-input />,
+      cols: { md: 12, xs: 24 },
+      itemProps: { required: true },
+      render: () => <ma-remote-select filterable />,
+      renderProps: {
+        api: () =>
+          new Promise((resolve) =>
+            resolve(remote({ channel_type: 1, support_collection: 1 }))
+          ),
+        dataHandle: (response: any) => {
+          return response.data?.map((item: ChannelDictVo) => {
+            return { label: `${item.channel_name}`, value: item.id };
+          });
+        },
+      },
     },
     {
-      label: t("TransactionParsingRules.regex"),
-      prop: "regex",
-      render: () => <el-input />,
-    },
-    {
-      label: t("TransactionParsingRules.variable_name"),
-      prop: "variable_name",
-      render: () => <el-input />,
+      label: t("TransactionParsingRules.status"),
+      prop: "status",
+      cols: { md: 12, xs: 24 },
+      itemProps: { required: true },
+      render: () => <ma-remote-select filterable />,
+      renderProps: {
+        api: () =>
+          new Promise((resolve) =>
+            resolve(selectStatus("transaction_parsing_rules", "status_list"))
+          ),
+        dataHandle: (response: any) => {
+          return response.data?.map((item: Common.StatusOptionItem) => {
+            return { label: `${item.label}`, value: item.value };
+          });
+        },
+        placeholder: t("TransactionParsingRules.status"),
+      },
     },
     {
       label: t("TransactionParsingRules.example_data"),
       prop: "example_data",
       render: () => <el-input />,
+      renderProps: {
+        type: "textarea",
+      },
     },
     {
-      label: t("TransactionParsingRules.status"),
-      prop: "status",
+      label: t("TransactionParsingRules.regex"),
+      prop: "regex",
       render: () => <el-input />,
+      renderSlots: {
+        prefix: () => <span style="margin-left: 8px">/</span>,
+        suffix: () => <span style="margin-right: 8px">/</span>,
+      },
+    },
+    {
+      label: t("TransactionParsingRules.variable_name"),
+      prop: "variable_name",
+      itemProps: { required: true },
+      render: () => <ma-remote-select filterable />,
+      renderProps: {
+        api: () =>
+          new Promise((resolve) =>
+            resolve(
+              selectStatus("transaction_parsing_rules", "variable_name_list")
+            )
+          ),
+        dataHandle: (response: any) => {
+          return response.data?.map((item: Common.StatusOptionItem) => {
+            return { label: `${item.label}`, value: item.value };
+          });
+        },
+        placeholder: t("TransactionParsingRules.variable_name"),
+        multiple: true,
+      },
+    },
+    {
+      label: t("TransactionParsingRules.parse_result"),
+      prop: "parse_result",
+      render: () => MaKeyValue,
+      renderProps: {
+        fixedKey: true,
+        disabled: true,
+      },
     },
   ];
 }
